7339f34e6bd7ff4439dddbd1f5d01059d2546f56
PlayerR9 <riateapad@gmail.com>
2024-10-19 11:15:19 +0200

    updated doc

--- control_flow.go
+++ control_flow.go
@@ -5,20 +5,42 @@ import (
 	"fmt"
 )
 
+// ErrPanic is an error that represents a panic.
 type ErrPanic struct {
+	// Value is the value of the panic.
 	Value any
 }
 
+// Error implements the error interface.
 func (e ErrPanic) Error() string {
-	return fmt.Sprintf("%v", e.Value)
+	return fmt.Sprintf("panic: %v", e.Value)
 }
 
+// NewErrPanic creates a new error that represents a panic.
+//
+// Parameters:
+//   - value: The value of the panic.
+//
+// Returns:
+//   - error: The new error. Never returns nil.
+//
+// Format:
+//
+//	"panic: <value>"
+//
+// where <value> is the value of the panic.
 func NewErrPanic(value any) error {
 	return &ErrPanic{
 		Value: value,
 	}
 }
 
+// try executes the given function and stores any paniced error in err.
+//
+// Parameters:
+//   - err: The error to store the paniced error in. If the given function
+//     does not panic, the value of err is not changed.
+//   - fn: The function to execute.
 func try(err *error, fn func()) {
 	defer func() {
 		r := recover()
@@ -39,6 +61,29 @@ func try(err *error, fn func()) {
 	fn()
 }
 
+// Try executes the given function and returns any paniced error. If the given
+// function does not panic, nil is returned.
+//
+// Parameters:
+//   - fn: The function to execute. If nil, nil is returned.
+//
+// Returns:
+//   - error: The paniced error, if any. Otherwise, nil.
+//
+// Errors:
+//   - *ErrPanic: If the panic value is not an error.
+//   - any other error: If the panic value is an error.
+//
+// If the panic value is an error, it is returned as-is. If the panic value is a string, it is
+// converted to an error. In any other case, a new ErrPanic is created.
+//
+// Example:
+//
+//	err := Try(func() {
+//		panic("something went wrong")
+//	})
+//
+//	fmt.Println(err) // Prints: "something went wrong"
 func Try(fn func()) error {
 	if fn == nil {
 		return nil
